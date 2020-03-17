a5f6715b0e50c731f47417912cbaf012629fb15d
testesoftwareufmg <testesoftwareufmg@gmail.com>
2020-03-16 21:24:36 -0300

    Processando Requisição POST

--- lists/tests.py
+++ lists/tests.py
@@ -20,3 +20,7 @@ def test_home_page_returns_correct_html(self):
     self.assertTrue(html.strip().endswith('</html>'))
 
     self.assertTemplateUsed(response, 'home.html') 
+	
+def test_can_save_a_POST_request(self):
+    response = self.client.post('/', data={'item_text': 'A new list item'})
+    self.assertIn('A new list item', response.content.decode())
